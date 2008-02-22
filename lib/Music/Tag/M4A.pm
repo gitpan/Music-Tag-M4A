0bc3f59349c5058de50c86a28d2234382d309bee
Edward J. Allen III <ealleniii@cpan.org>
2008-02-22 05:21:28 +0000

    Import of EALLENIII/Music-Tag-M4A-0.3 from CPAN.
    gitpan-cpan-distribution: Music-Tag-M4A
    gitpan-cpan-version:      0.3
    gitpan-cpan-path:         EALLENIII/Music-Tag-M4A-0.3.tar.gz
    gitpan-cpan-author:       EALLENIII
    gitpan-cpan-maturity:     released

--- lib/Music/Tag/M4A.pm
+++ lib/Music/Tag/M4A.pm
@@ -1,5 +1,5 @@
 package Music::Tag::M4A;
-our $VERSION = 0.28;
+our $VERSION = 0.30;
 
 # Copyright (c) 2007 Edward Allen III. Some rights reserved.
 #
@@ -28,7 +28,7 @@ Music::Tag::M4A - Plugin module for Music::Tag to get information from Apple Qui
 
 =head1 DESCRIPTION
 
-Music::Tag::M4A is used to read  header information from QuickTime MP4 contonainers. It uses Audio::M4P::Quicktime and MP4::Info.
+Music::Tag::M4A is used to read  header information from QuickTime MP4 contonainers. It uses Audio::M4P::QuickTime and MP4::Info.
 
 It is not currently able to write M4A tags.  Audio::M4P::QuickTime can write these tags, but iTunes has trouble reading them after
 they have been writen.  Setting the option "write_m4a" will enable some tags to be writen, but iTunes will have problems!
@@ -58,13 +58,26 @@ sub get_tag {
 	return $self;
 }
 
+sub qt {
+	my $self = shift;
+	unless (exists $self->{_qt}) {
+		$self->{_qt} =  Audio::M4P::QuickTime->new(file => $self->info->filename());
+	}
+	return $self->{_qt};
+}
+		
+
+
 sub get_tag_qt_info {
     my $self     = shift;
     my $filename = $self->info->filename();
-	my $qt = Audio::M4P::QuickTime->new(file => $filename);
-    my $tinfo    = $qt->iTMS_MetaInfo;
-	my $minfo    = $qt->GetMP4Info;
-	my $ginfo    = $qt->GetMetaInfo;
+	unless ($self->qt) {
+		$self->status("Failed to create Audio::M4P::QuickTime object");
+		return $self;
+	}
+    my $tinfo    = $self->qt->iTMS_MetaInfo;
+	my $minfo    = $self->qt->GetMP4Info;
+	my $ginfo    = $self->qt->GetMetaInfo;
     $self->info->album( $ginfo->{ALB} );
     $self->info->artist( $ginfo->{ART} );
 	my $date = $tinfo->{year} || $ginfo->{DAY};
@@ -74,13 +87,13 @@ sub get_tag_qt_info {
 
 =over 4
 
-=item artist, album 
+=item B<artist, album >
 
-=item disc, totaldiscs, tempo, encoder, title, composer
+=item B<disc, totaldiscs, tempo, encoder, title, composer>
 
-=item copyright, track, totaltracks, comment, lyrics
+=item B<copyright, track, totaltracks, comment, lyrics>
 
-=item bitrate, duration, picture
+=item B<bitrate, duration, picture>
 
 =cut
 
@@ -92,18 +105,18 @@ sub get_tag_qt_info {
 
     $self->info->tempo( $ginfo->{TMPO} );
     $self->info->encoder( $ginfo->{TOO} || "iTMS");
-	$self->info->genre( $qt->genre_as_text );
+	$self->info->genre( $self->qt->genre_as_text );
 	$self->info->title( $ginfo->{NAM} );
     $self->info->composer( $ginfo->{WRT} );
-	$self->info->track( $qt->track);
-	$self->info->totaltracks( $qt->total);
+	$self->info->track( $self->qt->track);
+	$self->info->totaltracks( $self->qt->total);
 	$self->info->comment($ginfo->{COMMENT});
 	$self->info->lyrics($ginfo->{LYRICS});
 
     $self->info->bitrate( $minfo->{BITRATE} );
     $self->info->duration( $minfo->{SECONDS} * 1000 );
 	if (not $self->info->picture_exists) {
-	  my $picture = $qt->GetCoverArt;
+	  my $picture = $self->qt->GetCoverArt;
 	  if ((ref $picture) && (@{$picture}) && ($picture->[0])) {
 		$self->info->picture( { "MIME type" => "image/jpg", "_Data" => $picture->[0] } );
 	  }
@@ -140,10 +153,13 @@ sub get_tag_mp4_info {
 sub set_tag {
     my $self     = shift;
     my $filename = $self->info->filename();
-	my $qt = Audio::M4P::QuickTime->new(file => $filename);
-    my $tinfo    = $qt->iTMS_MetaInfo;
-	my $minfo    = $qt->GetMP4Info;
-	my $ginfo    = $qt->GetMetaInfo;
+	unless ($self->qt) {
+		$self->status("Failed to create Audio::M4P::QuickTime object");
+		return $self;
+	}
+    my $tinfo    = $self->qt->iTMS_MetaInfo;
+	my $minfo    = $self->qt->GetMP4Info;
+	my $ginfo    = $self->qt->GetMetaInfo;
 	my $changed = 0;
 
 	if ($self->options->{write_m4a}) {
@@ -156,64 +172,102 @@ sub set_tag {
 
 	unless ($ginfo->{ALB} eq $self->info->album) {
 		$self->status("Storing new tag info for album");
-		$qt->SetMetaInfo(ALB => $self->info->album, 1, 'day');
+		$self->qt->SetMetaInfo(ALB => $self->info->album, 1, 'day');
 		$changed++;
     }
 	unless ($ginfo->{ART} eq $self->info->artist) {
 		$self->status("Storing new tag info for artist");
-		$qt->SetMetaInfo(ART => $self->info->artist, 1 , 'nam');
+		$self->qt->SetMetaInfo(ART => $self->info->artist, 1 , 'nam');
 		$changed++;
     }
 	unless ($ginfo->{TMPO} eq $self->info->tempo) {
 		$self->status("Storing new tag info for tempo");
-		$qt->SetMetaInfo(TMPO => $self->info->tempo, 1);
+		$self->qt->SetMetaInfo(TMPO => $self->info->tempo, 1);
 		$changed++;
     }
 	unless ($ginfo->{TOO} eq $self->info->encoder) {
 		$self->status("Storing new tag info for encoder");
-		$qt->SetMetaInfo(TOO => $self->info->encoder, 1, 'covr');
+		$self->qt->SetMetaInfo(TOO => $self->info->encoder, 1, 'covr');
 		$changed++;
     }
 	unless ($ginfo->{NAM} eq $self->info->title) {
 		$self->status("Storing new tag info for title");
-		$qt->SetMetaInfo(NAM => $self->info->title, 1, 'wrt');
+		$self->qt->SetMetaInfo(NAM => $self->info->title, 1, 'wrt');
 		$changed++;
     }
 	unless ($ginfo->{WRT} eq $self->info->composer) {
 		$self->status("Storing new tag info for composer");
-		$qt->SetMetaInfo(WRT => $self->info->composer, 1, 'alb');
+		$self->qt->SetMetaInfo(WRT => $self->info->composer, 1, 'alb');
 		$changed++;
     }
 	unless ($ginfo->{COMMENT} eq $self->info->comment) {
 		$self->status("Storing new tag info for comment");
-		$qt->SetMetaInfo(COMMENT => $self->info->comment, 1);
+		$self->qt->SetMetaInfo(COMMENT => $self->info->comment, 1);
 		$changed++;
     }
 	unless ($ginfo->{LYRICS} eq $self->info->lyrics) {
 		$self->status("Storing new tag info for lyrics");
 		my $lyrics = $self->info->lyrics;
 		$lyrics =~ s/\r?\n/\r/g;
-		$qt->SetMetaInfo(LYRICS => $self->info->lyrics, 1);
+		$self->qt->SetMetaInfo(LYRICS => $self->info->lyrics, 1);
 		$changed++;
     }
 	if ($changed) {
 		$self->status("Writing to $filename...");
-		$qt->WriteFile($filename);
+		$self->qt->WriteFile($filename);
 	}
     return $self;
 }
 
 sub close {
-
+	my $self = shift;
+	undef $self->{_qt};
+	delete $self->{_qt};
 }
 
 =back
+
+=head1 METHODS
+
+=over 4
+
+=item default_options
+
+Returns the default options for the plugin.  
+
+=item set_tag
+
+Save object back to MPEG4 container. THIS IS DANGEROUS. Requires write_m4a be set to true.
+
+=item get_tag
+
+Load information from MPEG4 container. 
+
+=item get_tag_qt_info 
+
+Load information using Audio::M4P::QuickTime
+
+=item get_tag_mp4_info
+
+Load information using MP4::Info
+
+=item close
+
+Close the file and destroy the Audio::M4P::QuickTime object. As this can be large, do this soon after running get_tag
+if you do not intend to write back to the file ever.
+
+=item qt
+
+Returns the Audio::M4P::QuickTime object
+
+=back
+
 
 =head1 OPTIONS
 
 =over 4
 
-=item write_m4a
+=item B<write_m4a>
 
 Set to true to allow some tags to be writen to disc.  Not recommended.
 
@@ -236,14 +290,15 @@ L<Audio::M4P::QuickTime>, L<MP4::Info>
 
 Edward Allen III <ealleniii _at_ cpan _dot_ org>
 
-
-=head1 COPYRIGHT
-
-Copyright (c) 2007 Edward Allen III. Some rights reserved.
+=head1 LICENSE
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the Artistic License, distributed
 with Perl.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007 Edward Allen III. Some rights reserved.
 
 
 =cut
